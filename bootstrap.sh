f56134a6a4a2afeebdc3cb81e33c6be1be6f097b
Lewis Peckover <lew@lew.io>
2014-11-02 14:46:34 +0000

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+function error_exit {
+    echo
+    echo "$@"
+    exit 1
+}
+shopt -s expand_aliases
+alias die='error_exit "Error at ${0}:`echo $(( $LINENO - 1 ))`:"'
+rpm -ihv https://bitbucket.org/lewispeckover/repo.lew.io/raw/master/el/6/noarch/lew-repo-1.0.0-1.noarch.rpm
+yum -y install puppet git || die "Failed to install required packages"
+cd /etc/puppet || die "Can't get to puppet directory /etc/puppet"
+git init
+git remote add origin https://bitbucket.org/lewispeckover/puppet.git
+git pull origin master
+git submodule update --init
+puppet apply --modulepath ./modules manifests/bootstrap.pp
