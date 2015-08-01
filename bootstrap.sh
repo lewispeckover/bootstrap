11a7ed492bf3c9f2336d775672b2b79bacc41dbc
Lewis Peckover <lew@lew.io>
2015-08-01 18:17:46 +0100

    add git installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ alias die='error_exit "Error at ${0}:`echo $(( $LINENO - 1 ))`:"'
 rpm --import https://bitbucket.org/lewispeckover/repo.lew.io/raw/master/RPM-GPG-KEY-LEW || die failed to fetch gpg key
 rpm -ihv https://bitbucket.org/lewispeckover/repo.lew.io/raw/master/el/6/noarch/lew-repo-1.1.0-1.noarch.rpm || die failed to install lew.io rpm repo
 yum -y install puppetlabs-release || die "Failed to install required packages"
-yum -y install puppet || die "Failed to install required packages"
+yum -y install puppet git || die "Failed to install required packages"
 cd /etc/puppet || die "Can't get to puppet directory /etc/puppet"
 git init
 git remote add origin https://bitbucket.org/lewispeckover/puppet.git
